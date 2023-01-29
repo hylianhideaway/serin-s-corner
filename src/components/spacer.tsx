d06868960fce56e557e7f39f8e2fe1e6c1a64dee
Austin Steltz <austinsteltz@gmail.com>
2023-01-29 15:33:03 -0600

    Create SpacerDiv component

---
+++ src/components/spacer.tsx
@@ -0,0 +1,16 @@
+import * as React from "react"
+
+
+/**
+ * React element used to add horizontal spacing between sections.
+ * Used throughout the website for uniform spacing needs. 
+ * @returns 
+ */
+const SpacerDiv: React.FC = () => {
+
+    return (
+      <div style={{height:"15px"}}/>
+    )
+  }
+
+export default SpacerDiv
