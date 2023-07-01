56f245ddec9819334ed4fa71533db1f0e40086ec
Austin Steltz <austinsteltz@gmail.com>
2023-06-30 23:49:20 -0500

    Add some stats for Tiberius

--- src/pages/characters/tiberius.tsx
+++ src/pages/characters/tiberius.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import type { HeadFC, PageProps } from "gatsby"
 import CharacterLayout from '../../components/characterLayout'
+import TraitsTable from '../../components/traitsTable'
 
 
 const pageTitle = "Tiberius"
@@ -10,15 +11,40 @@ const pageTitle = "Tiberius"
 const Tiberius: React.FC<PageProps> = () => {
   return (
     <CharacterLayout characterName="Tiberius">
-        Some additional info about tiberius here
-      
-
-
+        <TiberiusInternal/>
     </CharacterLayout>
 
 
   )
 }
+
+const TiberiusInternal: React.FC = () => {
+
+  const traitsTable1 : [string,string][] = 
+      [
+        ["Full Name","Tiberius Stormwind"],
+        ["Species","Blue Dragonborn"],
+        ["Gender","Male"],
+        ["Orientation","Gay"]
+      ];
+
+
+
+    return (
+      <div> {/*Outer DIV*/}
+
+            {/*General Traits Section*/}
+          <h3>Traits</h3> 
+          <div className="generalTraitsContainer">
+            <TraitsTable traitPairs={traitsTable1}/>
+          </div>
+      </div> 
+    )
+
+}
+
+
+
 
 export const Head: HeadFC = () => {
     return <title>{pageTitle}</title>
